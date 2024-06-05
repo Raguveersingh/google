f191f96d7cfef7136fd80416d0b8d6f3fad1e73e
Raguveersingh <165661102+Raguveersingh@users.noreply.github.com>
2024-06-05 10:39:09 +0530

    Update Controller.php on v1.1 05/06/2024

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+//this is a comment
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
